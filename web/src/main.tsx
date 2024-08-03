33b6776a27d2c1ffd11c389de0cfdac2d83d7653
unily <root@wanda.sb>
2024-08-03 11:06:31 +0800

    🧪 验证通过 webpack 触发

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,33 @@
-console.log("hello world");
+// import * as init from "../../pkg/rs_wgpu_engine";\
+// init.run();
+// console.log("init", init);
+// // init().then(() => {
+// //     console.log("WASM Loaded");
+// // });
+import wasmUri from "../../pkg/rs_wgpu_engine_bg.wasm";
+// @ts-ignore
+import { __wbg_set_wasm } from "../../pkg/rs_wgpu_engine_bg";
+
+console.log("wasmUri", wasmUri);
+function loadWasm() {
+    return fetch(wasmUri as any).then(response =>
+        response.arrayBuffer()
+    ).then(bytes =>
+        WebAssembly.compile(bytes)
+    ).then(module => {
+        return new WebAssembly.Instance(module);
+    });
+}
+
+async function main() {
+    const wasm = await loadWasm();
+    console.log(wasm);
+    // __wbg_set_wasm(wasm);
+    //
+    // wasm.__wbindgen_start();
+}
+
+main();
+
 
 export {};
