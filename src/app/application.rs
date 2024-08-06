15e2841561ad24ec35c98bd73f78dc1d3c230c03
unily <root@wanda.sb>
2024-08-06 18:00:57 +0800

    🧪 新增 app 入口包

---
+++ src/app/application.rs
@@ -0,0 +1,40 @@
+use winit::window::Window;
+
+struct Application<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    // The window must be declared after the surface so
+    // it gets dropped after it as the surface contains
+    // unsafe references to the window's resources.
+    window: &'a Window,
+}
+
+impl<'a> Application<'a> {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: &'a Window) -> Application<'a> {
+        todo!()
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
