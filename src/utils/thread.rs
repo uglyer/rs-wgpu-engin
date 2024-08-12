416b1b5e0833f3c1e2bf77dd16cc2d1560fc4ec6
unily <root@com.com.sb>
2024-08-12 16:44:07 +0800

    🧪 验证并发中断

--- src/utils/thread.rs
+++ src/utils/thread.rs
@@ -1,8 +1,17 @@
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 
 pub fn parallel<F>(start: usize, stop: usize, fnc: F)
+where
+    F: Fn(usize) + Send + Sync + 'static,
+{
+    let stop_signal = Arc::new(AtomicBool::new(false));
+    parallel_with_signal(stop_signal, start, stop, fnc)
+}
+
+pub fn parallel_with_signal<F>(stop_signal: Arc<AtomicBool>, start: usize, stop: usize, fnc: F)
 where
     F: Fn(usize) + Send + Sync + 'static,
 {
@@ -21,10 +30,15 @@ where
     for _ in 0..num_threads {
         let c = Arc::clone(&c);
         let fnc = Arc::clone(&fnc);
+        let stop_signal = Arc::clone(&stop_signal);
 
         let handle = thread::spawn(move || {
-            while let Some(i) = c.lock().unwrap().pop() {
-                fnc(i);
+            while !stop_signal.load(Ordering::Relaxed) {
+                if let Some(i) = c.lock().unwrap().pop() {
+                    fnc(i);
+                } else {
+                    break; // No more items to process
+                }
             }
         });
         handles.push(handle);
@@ -38,12 +52,12 @@ where
 #[test]
 fn test_parallel() {
     let mut hash_set: HashSet<usize> = HashSet::new();
-    for i in 0..1000 {
+    for i in 0..100000 {
         hash_set.insert(i);
     }
     let shared_set = Arc::new(Mutex::new(hash_set));
     let shared_set_clone = Arc::clone(&shared_set);
-    parallel(0,1000,move|i|{
+    parallel(0,100000,move|i|{
         let mut set = shared_set_clone.lock().unwrap();
         set.remove(&i);
     });
